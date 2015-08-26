759c2028292043bd833129321bde9a95b400889c
wsl <kkwsl@sina.com>
2015-08-26 21:36:28 +0800

    commit

---
+++ cpp16/cpp1627.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include "utlities.h"
+
+int main()
+{
+	
+	std::cout << compare(2, 1) << std::endl;
+	system("pause");
+	return 0;
+}
---
+++ cpp16/utlities.h
@@ -0,0 +1,11 @@
+#ifndef _UTLITIES_H_
+#define _UTLITIES_H_
+
+// 函数模板
+template <class T>
+int compare(const T&, const T&);
+
+// 函数的定义在utlities.cpp中
+#include "utlities.cc"
+
+#endif
