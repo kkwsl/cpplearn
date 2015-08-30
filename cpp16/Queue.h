b164722d26f93e477aec3d2749e6abad46380746
wsl <kkwsl@sina.com>
2015-08-30 10:41:25 +0800

    commit

--- cpp16/Queue.h
+++ cpp16/Queue.h
@@ -1,50 +1,59 @@
-#ifndef _QUEUE_H_
-#define _QUEUE_H_
+// declare Queue
+template <class Type> class Queue;
 
-#include "QueueItem.hpp"
-#include <iostream>
-
+// declare operator
 template <class T>
-class Queue
+std::ostream& operator<<(std::ostream&, const Queue<T>&);
+
+template <class Type> class QueueItem
 {
-public:
-	Queue():head(0),tail(0){}
+    friend class Queue<Type>;
+    friend std::ostream& operator<< <Type> (std::ostream&, const Queue<Type>&);
 
-	Queue(const Queue& q):head(0),tail(0)
-	{
-		copy_elems(q);
-	}
-
-	Queue& operator=(const Queue& rhs);
-	friend std::ostream& operator<< <T>(std::ostream&, const Queue<T>&); 
-
-	T& front(){ return head->item; }
-	const T& front() const { return head->item; }
-	void push(const T&);
-	void pop();
-	bool empty(){ return head == 0; };
-
-	~Queue()
-	{
-		destory();
-	}
-
-private:
-	QueueItem<T>* head, tail;
-
-	void destory();
-	void copy_elems(const Queue&);
+    QueueItem(const Type& t):item(t),next(0){}
+    Type item;
+    QueueItem* next;
 };
 
+template <class Type> class Queue
+{
+    friend std::ostream& operator<< <Type> (std::ostream&, const Queue&);
+public:
+    Queue():head(0),tail(0){}
+    template <class It>
+    Queue(It beg, It end):head(0),tail(0)
+    {
+        copy_elems(beg, end);
+    }
+
+    Queue(const Queue& q):head(0),tail(0)
+    { copy_elems(q);}
+
+    Queue& operator=(const Queue&);
+    ~Queue(){destroy();}
+
+    template <class Iter> void assign(Iter, Iter);
+
+    Type& front()
+    { return head->item; }
+    const Type& front() const
+    { return head->item; }
+    bool empty() const
+    { return head == 0; }
+
+    void push(const Type&);
+    void pop();
 
 
 
+private:
+    QueueItem<Type>* head;
+    QueueItem<Type>* tail;
 
+    void destroy();
+    void copy_elems(const Queue&);
 
+    template <class Iter> void copy_elems(Iter begin, Iter end);
+};
 
-
-
-
-
-
-#endif
+#include "Queue.cpp"
